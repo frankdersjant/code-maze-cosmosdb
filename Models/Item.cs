3d1efd867801d1da5bd76488e337a3a13a607f9d
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2021-09-18 14:00:29 +0200

    Add project files.

---
+++ Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace code_maze_cosmosdb.Models
+{
+    public class Item
+    {
+        [JsonPropertyName("id")]
+     
+        //NB NB id is with SMALL CAPITILIZATION - otherwise it wont work!!!
+        public string id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+    }
+}
